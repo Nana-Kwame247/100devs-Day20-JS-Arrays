554079a5c3b5cb584c6b7d1490c39ce8d77f0dee
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-04-20 05:02:31 +0300

    paradise after paradise completed

--- review-in-paradise-after-paradise/js/main.js
+++ review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,32 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function takeAnArray(x) {
+  // for (let i = 0; i < x.length; i++) {
+  // console.log(x[i]);
+  if (x[0] < x[x.length - 1]) {
+    alert(`Hi`);
+  } else if (x[0] > x[x.length - 1]) {
+    alert(`Bye`);
+  } else {
+    alert(`We close in an hour`);
+  }
+  //}
+}
+
+takeAnArray([1, 2, 3, 4, 5, 6]);
+takeAnArray([100, 2, 3, 4, 5, 6]);
+takeAnArray([6, 2, 3, 4, 5, 6]);
+
+// solving the same problem with ForEach
+
+// lessThanLastNumber.forEach((x) => {
+//   if (x[0] < x[x.length - 1]) {
+//     alert(`Hi`);
+//   } else if (x[0] > x[x.length - 1]) {
+//     alert(`Bye`);
+//   } else {
+//     alert(`We close in an hour`);
+//   }
+// });
+
+// lessThanLastNumber([1, 2, 3, 4, 5, 6, 7]);
--- review-summer-games/js/main.js
+++ review-summer-games/js/main.js
@@ -1,1 +1,18 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+
+function multiplyEachOther(num) {
+  let multiply = 1;
+  for (let i = 0; i < num.length; i++) {
+    multiply = multiply * num[i];
+    console.log(multiply);
+  }
+}
+
+multiplyEachOther([1, 2, 3, 4, 5, 6]);
+
+//Leon's solution
+
+function multiNumInArr(arr) {
+  let product = 1;
+  arr.forEach((num) => (product *= num));
+}
